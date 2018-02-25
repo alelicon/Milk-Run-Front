869dd5db40e0120875d7b8251acb840ddfce3f72
Daniel Ornelas <dornelas4@miners.utep.edu>
2018-02-25 02:10:51 -0700

    REAL TIME TRACKING

--- MovingSquare.js
+++ MovingSquare.js
@@ -1,5 +1,6 @@
-var serverURL;
+var serverURL = "http://159.89.231.140/updatedmag";
 var svg = d3.select("svg");
+//var dummyData = [[95,20],[100,20],[90,20]];
 var button = d3.select("#move");
 var getButon = d3.select("#db");
 var mySquare=svg.append("rect")
@@ -23,20 +24,17 @@ mySquare
   .style("opacity",1);
 
 
-
+/*Request latest coordinates every second and update figure position
+*
+*/
 setInterval(function (){
+	console.log("SEND GECT");
     $.get(serverURL, function(data, status){
-		var moveData = JSON.parse(data);
-		var xMov = moveData.x;
-		var yMov = moveData.y;
-        alert("Data: " + moveData + "\nStatus: " + status);
-		mySquare.transition().attr("x",xMov).delay(1000);
-		mySquare.transition().attr("y",yMov).delay(1000);
+		mySquare.transition().attr("x",data.x).delay(1000);
+		mySquare.transition().attr("y",data.y).delay(1000);
     });
 }, 1000);
 
 
 
-
-  
  
